929dd9c208b0cd87be8e6a72b725fba9d99389fe
TFakeye6943 <Tfakeye6943@sohamcollege.org.uk>
2014-05-15 13:11:02 +0100

    Update diceRoller.py

--- diceRoller.py
+++ diceRoller.py
@@ -1,7 +1,7 @@
 #Coin flip program
 #Describe the purpose of this program here.
+import random,time
 
-inport random;time
 
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
@@ -10,25 +10,30 @@ s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
 s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
+roll=(0)
 def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
+    print("rolling.....")
+    roll = random.randint(1,7)
+    return roll
 
-
-def show_dice(roll)
-    if roll = 1:
+def show_dice():
+    roll=(0)
+    question=input("What do you want to roll- ")
+    if roll == 1:
         print(S1)
-    else roll = 2
+    elif roll == 2:
         print(s2)
-    else roll = 3
+    elif roll== 3:
         print(s3)
-    else roll = 4
+    elif roll == 4:
         print(s4)
-    else roll = 5
+    elif roll == 5:
         print(s5)
-    else roll = 6
+    else:
         print(s6)
 
-roll
+Finalroll=roll()and show_dice()
+   
+roll()
 time.sleep(1)
-show_dice(roll)
+show_dice()
