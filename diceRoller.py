be5bb60691bd6345c8316e3c808400d48cea12fb
TFakeye6943 <Tfakeye6943@sohamcollege.org.uk>
2014-05-21 20:39:53 +0100

    Update diceRoller.py

--- diceRoller.py
+++ diceRoller.py
@@ -3,8 +3,15 @@
 import random,time
 
 
+#Coin flip program
+#Describe the purpose of this program here.
+#This code should ask the user for input and then roll the number they wanted to roll
+
+import random,time
+
+
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
-s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
+s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"#This list prints the side of the die
 s3 = "- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n"
 s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
 s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
@@ -13,14 +20,14 @@ s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 roll=(0)
 def roll():
     print("rolling.....")
-    roll = random.randint(1,7)
+    roll = random.randint(1,6)#This function makes sure you can only ask for a number from 1-6
     return roll
+rolling=roll()
 
 def show_dice():
-    roll=(0)
-    question=input("What do you want to roll- ")
+    roll=int(input("What do you want to roll- "))# Thnis function asks the user for input on how much they want to roll then prints out the correct side of the die
     if roll == 1:
-        print(S1)
+        print(s1)
     elif roll == 2:
         print(s2)
     elif roll== 3:
@@ -32,8 +39,12 @@ def show_dice():
     else:
         print(s6)
 
-Finalroll=roll()and show_dice()
-   
-roll()
+dice=show_dice()
 time.sleep(1)
-show_dice()
+
+#Errors
+#no indentation
+#Always rolls a six
+#Should only print 1-6 not 1-7
+#S1 instead of s1
+
